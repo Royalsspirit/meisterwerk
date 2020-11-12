63a527c29132ade78db655da39ea57b6d461f541
gmajkic <gmajkic@vente-privee.com>
2020-11-12 19:38:12 +0100

    feat(List): move move class component to classic function and use hooks

--- website/src/components/App.js
+++ website/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-import List from './List.container'
+import List from './List'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
