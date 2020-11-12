f3f83d5de8db9b1480686f6cb64f6b10818137f3
gmajkic <gmajkic@vente-privee.com>
2020-11-12 19:02:28 +0100

    feat(api): define a struct which implement http.Transport to add authorization token github

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/Royalsspirit/meisterwerk/internal/api"
 	"net/http"
+
+	"github.com/Royalsspirit/meisterwerk/internal/api"
+	"github.com/joho/godotenv"
 )
 
+// AddHeaderTransport AddHeaderTransport
+type AddHeaderTransport struct{}
+
+//RoundTrip RoundTrip
+func (adt *AddHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if os.Getenv("GITHUB_TOKEN") != "" {
+		req.Header.Add("Authorization", "token "+os.Getenv("GITHUB_TOKEN"))
+	}
+	fmt.Println("header", req.Header)
+	return http.DefaultTransport.RoundTrip(req)
+}
+
 func main() {
+	godotenv.Load()
+
 	conf := api.Server{
-		RedisHost:  os.Getenv("REDIS_HOST"),
-		Port:       "8000",
-		Env:        "dev",
-		HttpClient: http.Client{},
+		RedisHost: os.Getenv("REDIS_HOST"),
+		Port:      "8000",
+		Env:       "dev",
+		HttpClient: http.Client{
+			Transport: &AddHeaderTransport{},
+		},
 	}
 	server := api.NewServer(&conf)
 	server.Run()
