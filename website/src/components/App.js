533b915bc70f48a38747926927c5f070a134224c
gmajkic <gmajkic@vente-privee.com>
2020-11-13 11:54:20 +0100

    fix(App): lint it

--- website/src/components/App.js
+++ website/src/components/App.js
@@ -3,17 +3,13 @@ import logo from './logo.svg'
 import './App.css'
 import List from './List'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 const App = () => {
     return (
         <div className="App">
-
-<header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" />
-                        </header>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
             <Container fluid>
-
                 <List />
             </Container>
         </div>
